bd1d0332a99f367384811fade4b08882211f3cef
Ben Cunningham <benjamescunningham@gmail.com>
2018-11-04 22:10:22 -0500

    Refactor subs class

--- subsets/subs.py
+++ subsets/subs.py
@@ -1,36 +1,41 @@
 import srt
 import pydub
 
-class SRT():
+class Subset():
 
-    def __init__(self, path, **kwargs):
+    def __init__(self, subs=None, audio=None):
 
-        self.path = path
-        self.subs = self.read(path, **kwargs)
+        self.subs = subs
+        self.audio = audio
 
 
-    @staticmethod
-    def read(path, **kwargs):
+    def read_subs(self, path, **kwargs):
 
         with open(path, "r", **kwargs) as file:
             subs = list(srt.parse(file.read()))
 
-        return subs
+        self.subs = subs
+
+        return self
 
 
-    def split_file(self, path, **kwargs):
+    def read_audio(self, path, **kwargs):
 
-        audio = pydub.AudioSegment.from_file(path, **kwargs)
+        self.audio = pydub.AudioSegment.from_file(path, **kwargs)
+
+        return self
+
+
+    def split_audio(self):
 
         msec = lambda time: time.total_seconds() * 1000
         stamp = lambda sub: (msec(sub.start), msec(sub.end))
+        splice = lambda split: self.audio[split[0]:split[1]]
 
         splits = [stamp(sub) for sub in self.subs]
+        audio = [splice(split) for split in splits]
 
-        splice = lambda split: audio[split[0]:split[1]]
-        audios = [splice(split) for split in splits]
-
-        return audios
+        return audio
 
 
     def __len__(self):
