3d257ed35cb8ff8387dfe8e46ef6a18d3f7a86db
Ben Cunningham <benjamescunningham@gmail.com>
2018-11-07 20:00:18 -0500

    Refactor yet again, plus docstrings

--- subsets/subset.py
+++ subsets/subset.py
@@ -1,10 +1,17 @@
 import srt
 import pydub
+from subset.utils import bound, msec
 
 
 class Subset():
 
     def read_subs(self, path, **kwargs):
+        """Read subtitle file
+
+        Reads a subtitle file into the object as a Subtitle object. This
+        is just a thin wrapper for srt.parse. Pass additional kwargs
+        like the file encoding to properly handle opening the file.
+        """
 
         with open(path, "r", **kwargs) as file:
             subs = list(srt.parse(file.read()))
@@ -15,6 +22,12 @@ class Subset():
 
 
     def read_audio(self, path, **kwargs):
+        """Read audio file
+
+        Reads an audio file into the object. This is just a thin wrapper
+        for pydub.AudioSegment.from_file, and by passing kwargs any file
+        type handled by ffmpeg can be loaded.
+        """
 
         self.audio = pydub.AudioSegment.from_file(path, **kwargs)
 
@@ -22,15 +35,28 @@ class Subset():
 
 
     def split_audio(self):
+        """Split audio on subtitle bounds
 
-        msec = lambda time: time.total_seconds() * 1000
-        stamp = lambda sub: (msec(sub.start), msec(sub.end))
-        splice = lambda split: self.audio[split[0]:split[1]]
+        Splits the object's audio into separate objects corresponding to
+        the timestamps of every subtitle.
+        """
 
-        splits = [stamp(sub) for sub in self.subs]
-        self.splits = [splice(split) for split in splits]
+        bounds = [bound(sub) for sub in self.subs]
+        self.splits = [self.splice(bound) for bound in bounds]
 
         return self
+
+
+    def _split(self, bound):
+        """Split audio
+
+        Split the object's audio given a tuple of bounds (i.e., start
+        and end timestamps).
+        """
+
+        start, end = bound
+
+        return self.audio[start:end]
 
 
     def __len__(self):
