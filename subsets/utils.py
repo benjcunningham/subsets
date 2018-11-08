ee759da62fac8335e27367c08102754fae563a6e
Ben Cunningham <benjamescunningham@gmail.com>
2018-11-07 19:18:05 -0500

    Move lambdas to utils

---
+++ subsets/utils.py
@@ -0,0 +1,17 @@
+def msec(time):
+    """Convert to milliseconds
+
+    Convert a timedelta object to milliseconds.
+    """
+
+    return time.total_seconds() * 1000
+
+
+def bound(sub):
+    """Time bound a subtitle
+
+    Given a Subtitle object, return a tuple containing its start and end
+    times.
+    """
+
+    return (msec(sub.start), msec(sub.end))
